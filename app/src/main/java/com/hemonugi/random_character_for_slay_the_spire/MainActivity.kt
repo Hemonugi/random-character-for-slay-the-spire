886425ee7409fc199d081bef14e4ba1cb46db233
hemonugi <hemonugi@gmail.com>
2022-12-05 18:51:02 +0400

    added random image upload

--- app/src/main/java/com/hemonugi/random_character_for_slay_the_spire/MainActivity.kt
+++ app/src/main/java/com/hemonugi/random_character_for_slay_the_spire/MainActivity.kt
@@ -2,10 +2,21 @@ package com.hemonugi.random_character_for_slay_the_spire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = this.findViewById<ImageView>(R.id.imageView)
+        val listCharacters = listOf(
+            R.drawable.ironclad,
+            R.drawable.silent,
+            R.drawable.defect,
+            R.drawable.watcher,
+        )
+
+        imageView.setImageResource(listCharacters.random())
     }
 }
