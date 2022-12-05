cf49544b2ccfb9eee068351145753389bf9d6a8a
hemonugi <hemonugi@gmail.com>
2022-12-05 17:19:45 +0400

    initial commit

---
+++ app/src/main/java/com/hemonugi/random_character_for_slay_the_spire/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hemonugi.random_character_for_slay_the_spire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
